850f6b23952a9ed6420f63a8ba93443a273aeb47
Simon <sipe0005@stud.kea.dk>
2026-02-08 13:40:11 +0100

    removed dobble line

--- src/backend/app.py
+++ src/backend/app.py
@@ -30,10 +30,6 @@ app = Flask(__name__)
 
 app.secret_key = SECRET_KEY
 
-app = Flask(__name__)
-
-app.secret_key = SECRET_KEY
-
 
 ################################################################################ 
 # Database Functions
